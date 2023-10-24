6ea426397720072b2513d4edc93c50ba6f2eea24
lazarcon <constantin.lazari@gmail.com>
2023-10-24 14:39:56 +0200

    Optimized session creation

--- utils/database.py
+++ utils/database.py
@@ -3,22 +3,27 @@ from sqlalchemy.orm import sessionmaker, Session
 
 from .models import Base
 
-engine = create_engine("sqlite:///media.sqlite")
+# Create and configure the engine with a connection pool
+engine = create_engine("sqlite:///media.sqlite", pool_pre_ping=True, pool_size=10, max_overflow=20)
 
 
-@event.listens_for(Engine, "connect")
+@event.listens_for(engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
 
+# Use a context manager for session creation
+Session = sessionmaker(bind=engine)
+
+
 def get_session() -> Session:
     """
     Returns a fresh database session
     """
-    session = sessionmaker(bind=engine)
-    return session
+    with Session() as session:
+        return session
 
 
 def create_tables() -> None:
