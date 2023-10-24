be58e432c7dbf7b27f3ec61a434dbf8cea373609
lazarcon <constantin.lazari@gmail.com>
2023-10-24 09:28:16 +0200

    simplified main script

--- app.py
+++ app.py
@@ -6,14 +6,9 @@ from pprint import pprint
 
 from utils.file import load_config
 from utils.media_manager import MediaManager
-from utils.database import create_tables
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.NOTSET)
-
-
-def setup(config):
-    create_tables()
 
 
 def main(arguments):
@@ -26,10 +21,6 @@ def main(arguments):
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-s", "--setup", action="store_true",
-                        help="Creates all necessary tables")
-    parser.add_argument("-u", "--update", action="store_true",
-                        help="Update Notion Database from local storage")
     parser.add_argument("-d", "--debug", action="store_true",
                         help="Run in debug mode")
     parser.add_argument("-i", "--info", action="store_true",
@@ -38,21 +29,13 @@ def main(arguments):
 
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
-    if args.info:
+    elif args.info:
         logging.basicConfig(level=logging.INFO)
 
     config = load_config()
-
-    if args.setup:
-        setup(config)
-
-    if args.update:
-        app = MediaManager(config)
-        app.update()
+    app = MediaManager(config)
+    app.run()
 
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
-
-
-
