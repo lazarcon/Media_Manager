f53e2c8cf8e4065e06a12070b1ae64f02392e20e
lazarcon <constantin.lazari@gmail.com>
2023-10-25 08:46:21 +0200

    made arguments more flexibel

--- app.py
+++ app.py
@@ -18,9 +18,19 @@ def main(arguments):
     Args:
         arguments (list): Command-line arguments passed to the script.
     """
+
+    config = load_config()
+    local_media_locations = config["local_media_locations"]
+    movie_locations = local_media_locations["movies"]
+    movie_location_choices = [location["label"].lower() for location in movie_locations] + ["all"]
+
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-m", "--movies",
+                        choices=movie_location_choices,
+                        type=str.lower, nargs="?", const="all")
+    # (1/3) Add more media handling options here to come
     parser.add_argument("-d", "--debug", action="store_true",
                         help="Run in debug mode")
     parser.add_argument("-i", "--info", action="store_true",
@@ -32,9 +42,20 @@ def main(arguments):
     elif args.info:
         logging.basicConfig(level=logging.INFO)
 
-    config = load_config()
     app = MediaManager(config)
-    app.run()
+    if (
+        not args.movies
+        # (2/3) and more missing media handling arguments here
+    ):
+        app.run(movie_locations=movie_locations)
+    else:
+        if args.movies:
+            if "all" in args.movies:
+                locations = movie_locations
+            else:
+                locations = [movie_location for movie_location in movie_locations if movie_location["label"].lower() in args.movies]
+            app.run(movie_locations=locations)
+        # (3/3) and add more actual media handling here
 
 
 if __name__ == "__main__":
