9f48acadfb1313002f9db3342230622056468c61
lazarcon <constantin.lazari@gmail.com>
2023-10-27 14:24:52 +0200

    removed unused parameter

--- utils/media_manager.py
+++ utils/media_manager.py
@@ -12,7 +12,6 @@ class MediaManager:
         self.movies_manager = MovieManager(
             api_key = config["notion_api_key"],
             movie_database_id=config["notion_media"]["movies"]["movie_db"],
-            genre_database_id=config["notion_media"]["movies"]["genre_db"],
             omdb_api_key=config["omdb_api_key"]
         )
 
